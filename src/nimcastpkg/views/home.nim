220899d01c0e2559d697715fbd4be29587b46fdd
Konstantin Molchanov <moigagoo@live.com>
2017-01-17 23:44:46 +0400

    Views: Rename homepage view.

---
+++ src/nimcastpkg/views/home.nim
@@ -0,0 +1,111 @@
+#? stdtmpl
+#
+#import ../db
+#
+#proc renderHome*(): string =
+#  result = ""
+<!DOCTYPE HTML>
+<!--
+	Dopetrope by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>The Nim Lang Podcast | Podcast about Nim programming language</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+					<div id="header">
+
+						<!-- Logo -->
+							<h1>The Nim Lang Podcast</h1>
+
+						<!-- Banner -->
+							<section id="banner">
+								<header>
+									<h2>Latest Episode</h2>
+									<p>This is the latest episode</p>
+								</header>
+							</section>
+
+					</div>
+				</div>
+
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div class="container">
+						<div class="row">
+							<div class="12u">
+
+								<!-- Portfolio -->
+									<section>
+										<header class="major">
+											<h2>Episodes</h2>
+										</header>
+										<div class="row">
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+													<header>
+														<h3>Ipsum feugiat et dolor</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Find out more</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+													<header>
+														<h3>Sed etiam lorem nulla</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Find out more</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+													<header>
+														<h3>Consequat et tempus</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Find out more</a>
+													</footer>
+												</section>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
