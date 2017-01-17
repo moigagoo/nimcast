9eaad82d85242b30a8a8ea813bec0c936fbe08c7
Konstantin Molchanov <moigagoo@live.com>
2017-01-17 10:55:23 +0400

    Add db interface.

---
+++ src/nimcastpkg/db.nim
@@ -0,0 +1,43 @@
+import times
+import db_sqlite
+import json
+
+type
+  Episode* = object
+    title*: string
+    tagline*: string
+    guest*: string
+    timestamp*: Time
+    notes*: seq[string]
+    tags*: seq[string]
+
+type
+  Db = ref object
+    connection: DbConn
+
+proc newDb*(filename = "nimcast.db"): Db =
+  new result
+  result.connection = open(filename, "", "", "")
+
+proc init*(db: Db) =
+  db.connection.exec(
+    sql"""
+      CREATE TABLE IF NOT EXISTS Episode(
+        title text PRIMARY KEY,
+        tagline text,
+        guest text,
+        timestamp integer NOT NULL,
+        notes text,
+        tags text
+      );
+    """
+  )
+
+proc close*(db: Db) = db.connection.close()
+
+proc add*(db: Db, episode: Episode) =
+  db.connection.exec(
+    sql"INSERT INTO Episode VALUES (?, ?, ?, ?, ?, ?);",
+    episode.title, episode.tagline, episode.guest,
+    int(episode.timestamp), %episode.notes, %episode.tags
+  )
